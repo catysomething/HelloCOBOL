924b61c0608acc6be67e0ccfc295a1a15ac12ac9
Caty Chapman <caty@cherkesky.com>
2020-04-25 15:50:05 -0500

    added arrays file

--- hello.cbl
+++ hello.cbl
@@ -1,11 +1,15 @@
-
             *> setup the identification division
             IDENTIFICATION DIVISION.
             *> setup the program id
             PROGRAM-ID. HELLO.
             *> setup the procedure division (like 'main' function)
+            DATA DIVISION.
+              *> working storage defines variables
+              WORKING-STORAGE SECTION.
+              01 FIRST-VAR PIC A(5) VALUE 'WORLD'.
+            
             PROCEDURE DIVISION.
               *> print a string
-              DISPLAY 'HELLO WORLD'.
+              DISPLAY "HELLO "FIRST-VAR.
             *> end our program
             STOP RUN.
